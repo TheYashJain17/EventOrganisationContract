2623359b3815ba971b15b68cd4b9748bf68a039d
yash jain <yashjain07.yj@gmail.com>
2022-12-23 12:24:26 +0530

    Made A registerEvent Function To Register/Create An Event

--- EventOrganisation.sol
+++ EventOrganisation.sol
@@ -12,7 +12,7 @@ contract eventOrg{
         uint price;
         uint id;
         uint totalTickets;
-        uint ticketRemaining;
+        uint remainingTickets;
 
     }
 
@@ -21,13 +21,46 @@ Event[] allEvents;
 mapping(uint => Event) public Events;
 mapping(address => mapping(uint => uint)) ticketBal;
 
+address owner;
 uint eventId;
+
+constructor(){
+    owner = msg.sender;
+}
 
 function allEventsInfo() view external returns(Event[] memory){
  
     return allEvents;
 
 }
+
+function registerEvent(string memory _name , uint _date , string memory _place , uint _price , uint _totalTickets , uint _remainingTickets) external{
+
+    require(msg.sender == owner,"Only Owner Can Register An Event");
+
+    require(_date > block.timestamp,"You Can Organize Events For Future Date Only");
+
+    require(_totalTickets > 0,"You Have To Create More Than 0 Tickets");
+
+    Event memory _event;
+
+    _event.name = _name;
+    _event.date = _date;
+    _event.place = _place;
+    _event.price = _price;
+    _event.id = eventId;
+    _event.totalTickets = _totalTickets;
+    _event.remainingTickets = _remainingTickets;
+
+    Events[eventId] = _event;
+
+    allEvents.push(_event);
+
+    eventId++;
+
+}
+
+
 
 
 
